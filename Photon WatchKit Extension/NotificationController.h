25f48671d6770a44a0d14946ee68e426103fd9d4
Phil Webster <webster.phil@gmail.com>
2015-01-22 09:57:27 -0800

    Add watch kit back.

---
+++ Photon WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Photon WatchKit Extension
+//
+//  Created by Philip Webster on 1/22/15.
+//  Copyright (c) 2015 phil. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
