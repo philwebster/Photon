4a142b516612b5691ec5bfae3b2c7b43ec7b56ff
Phil Webster <webster.phil@gmail.com>
2015-05-24 11:44:28 -0700

    Copy OS X starter app into project

--- Photon (OS X)/AppDelegate.h
+++ Photon (OS X)/AppDelegate.h
@@ -1,15 +1,41 @@
 //
 //  AppDelegate.h
-//  Photon (OS X)
+//  HueQuickStartApp-OSX
 //
-//  Created by Philip Webster on 4/25/15.
-//  Copyright (c) 2015 phil. All rights reserved.
+//  Created by Paul Verhoeven on 11/5/13.
+//  Copyright (c) 2013 Philips. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+#include "PHBridgePushLinkViewController.h"
+#include "PHBridgeSelectionViewController.h"
+#import <HueSDK_OSX/HueSDK.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+#define NSAppDelegate  ((AppDelegate *)[[NSApplication sharedApplication] delegate])
+
+@class PHHueSDK;
+
+@interface AppDelegate : NSObject <NSApplicationDelegate,PHBridgePushLinkViewControllerDelegate,PHBridgeSelectionViewControllerDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property (strong, nonatomic) PHHueSDK *phHueSDK;
+
+#pragma mark - HueSDK
+
+/**
+ Starts the local heartbeat
+ */
+- (void)enableLocalHeartbeat;
+
+/**
+ Stops the local heartbeat
+ */
+- (void)disableLocalHeartbeat;
+
+/**
+ Starts a search for a bridge
+ */
+- (void)searchForBridgeLocal;
 
 
 @end
-
